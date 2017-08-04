b194f8eda416f9f40613168e9fdebcf18ce41528
Alexander Ding <alexding123@outlook.com>
2017-08-03 22:34:38 -0400

    Final

--- Augmented Nutrition Scanner/Assets/scripts/Unique.cs
+++ Augmented Nutrition Scanner/Assets/scripts/Unique.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Unique {
 	static public string ApiKey = "c0AxLI8FC6mshMcrq5buOTtMbZnhp1Yn22SjsnHQ3za3k7XBQG";
-	// static public string Home = "https://nutritionix-api.p.mashape.com/v1_1";
-	static public string Home = "localhost:1234/api";
+	static public string Home = "https://nutritionix-api.p.mashape.com/v1_1";
+	// static public string Home = "localhost:1234/api";
 	static public Hashtable PopUps = new Hashtable();
     static public Hashtable Displays = new Hashtable();
 	static public Dictionary<string, BarInfo> BarInfos = new Dictionary<string, BarInfo>() {
